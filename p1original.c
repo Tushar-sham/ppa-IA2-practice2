7529251ebfda3c42fd083293f3dd15360b2f8e2f
Tushar <94678094+Tushar-sham@users.noreply.github.com>
2022-02-07 09:43:42 +0000

    printf("invalid input\n");

--- p1original.c
+++ p1original.c
@@ -1,2 +1,21 @@
 
+
+#include<stdio.h>
+int main()
+{
+  float base, height, area;
+  printf("Enter length of base of triangle\n");
+  scanf("%f", &base);
+  printf("Enter lenght of height of triangle\n");
+  scanf("%f", &height);
+  if(base == 0 || height == 0)
+{
+  printf("invalid input\n");
+}
+else
+{
+  area = (base*height)/2.0;
+  printf("area of triangle is: %0.2f\n", area);
+}
+return 0;
 }
